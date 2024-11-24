a150e14e69a708d5b66886e8e20e76b48adf9cdc
Farzin Ghanbari <tfarzin@gmail.com>
2024-11-24 11:37:20 +0330

    Update sqlite_connection.py

--- test/sqlite_connection.py
+++ test/sqlite_connection.py
@@ -1,75 +1,95 @@
-from contextlib import AbstractContextManager
-from typing import Union
+from contextlib import contextmanager
+from typing import Generator
 
-from sqlalchemy import Engine, create_engine, text
-from sqlalchemy.orm import Session, sessionmaker, DeclarativeMeta
-from sqlalchemy.util import FacadeDict
+from apsc.base import Base
+from apsc.sql_server_database_connection.isql_server_database_connection import IMSSQLServerConnection
+from sqlalchemy import Engine, create_engine, text, StaticPool
+from sqlalchemy.dialects.mssql import BIGINT, DATETIME, DATETIME2, DATE, DECIMAL, TINYINT
+from sqlalchemy.dialects.sqlite import INTEGER, DATETIME as SQLITE_DATETIME, DATE as SQLITE_DATE, \
+    DECIMAL as SQLITE_DECIMAL, SMALLINT
+from sqlalchemy.orm import sessionmaker, DeclarativeMeta, class_mapper, scoped_session
 
-from src.company_package import IDatabaseConnection
-from src.sqlalchemy_base import Base
 
-
-class SqliteConnection(IDatabaseConnection):
+class SqliteConnection(IMSSQLServerConnection):
     def __init__(self, models: list = None):
         super().__init__()
-        self.__engine: Engine = self.__create_engine()
-        self.__migrate_tables()
-        self.__session_factory = sessionmaker(self.__engine, autoflush=True)
         self.__models = models
-        self.__auxiliary_session = self.__session_factory()
+        self.__schema_set = set()
+
+        self.__engine: Engine = create_engine('sqlite:///:memory:', connect_args={'check_same_thread': False},
+                                              poolclass=StaticPool)
+        self.__connection = self.__engine.connect()
+        self.attach_all_the_schemas()
+        session_factory = sessionmaker(autoflush=True, bind=self.__engine)
+        self.__Session = scoped_session(session_factory)
+        self.__migrate_tables()
         self.__seed_models()
 
-    def __del__(self):
-        self.__auxiliary_session.close_all()
-        self.__session_factory.close_all()
+    def attach_all_the_schemas(self):
+        for model in Base.__subclasses__():
+            self.__set_type_of_columns_of_sqlite_to_columns(model)
+            table = getattr(model, '__table__', None)
+            if table is not None:
+                if schema := getattr(table, 'schema', False):
+                    self.__schema_set.add(schema)
+        for schema in self.__schema_set:
+            self.__connection.execute(text(f"attach ':memory:' as {schema}"))
 
-    def sqlalchemy_session(self, has_transaction=False) -> Union[AbstractContextManager[Session], Session]:
+    def close(self):
+        self.__Session.close_all()
+        self.__connection.close()
+        self.__engine.dispose()
+
+    @contextmanager
+    def ms_sql_server_session(self, has_transaction=False) -> Generator:
+        yield self.__Session
         if has_transaction:
-            return self.__session_factory.begin()
-        return self.__session_factory(expire_on_commit=False)
+            self.__Session.commit()
+        self.__Session.remove()
 
-    def query_session(self):
-        return self.__auxiliary_session
+    def get_table_names_in_schema(self, schema_name):
+        with self.ms_sql_server_session() as session:
+            table_names = session.execute(
+                text(f"SELECT name FROM {schema_name}.sqlite_master WHERE type='table';")).scalars().all()
+        return table_names
+
+    def get_all_table_names(self):
+        with self.ms_sql_server_session() as session:
+            table_names = session.execute(text(f"SELECT name FROM sqlite_master WHERE type='table';")).scalars().all()
+
+        for schema_name in self.__schema_set:
+            schema_tables = self.get_table_names_in_schema(schema_name)
+            for table_name in schema_tables:
+                table_names.append(f'{schema_name}.{table_name}')
+        return table_names
 
     def __seed_models(self) -> None:
         if self.__models:
-            with self.__session_factory.begin() as session:
+            with self.ms_sql_server_session() as session:
                 session.add_all(self.__models)
                 session.commit()
 
-    def __contains_any_tables(self) -> bool:
-        with self.__session_factory() as session:
-            tables = session.execute(text("SELECT name FROM sqlite_master WHERE type='table';")).all()
-        return tables != []
-
-    def __create_engine(self) -> Engine:
-        return create_engine(f'sqlite:///file:?mode=memory&cache=shared&uri=true', )
-
     def __migrate_tables(self) -> None:
         self.__base_model: DeclarativeMeta = Base
-        self.__delete_extra_metadata_from_models(self.__base_model)
-        self.__delete_schema_from_tables_metadata_of_base_tables(self.__base_model)
-        self.__base_model.metadata.reflect(self.__engine, schema=None)
         self.__base_model.metadata.create_all(self.__engine)
 
-    @staticmethod
-    def __delete_extra_metadata_from_models(base_model: DeclarativeMeta) -> None:
-        for model in base_model.__subclasses__():
-            if _ := getattr(model, '__table_args__', None):
-                delattr(model, '__table_args__')
-            table = getattr(model, '__table__', None)
-            if table is not None:
-                if _ := getattr(table, 'schema', False):
-                    setattr(table, 'schema', None)
-                    setattr(model, '__table__', table)
+    def __set_type_of_columns_of_sqlite_to_columns(self, model):
+        mapper = class_mapper(model)
+        for column in mapper.columns:
+            self.__get_sqlite_type_for_column(column)
 
     @staticmethod
-    def __delete_schema_from_tables_metadata_of_base_tables(base_model: DeclarativeMeta) -> None:
-        current_schema = ''
-        if schemas := list(base_model.metadata._schemas):
-            current_schema = schemas[0]
-        tables = {}
-        for table in base_model.metadata.tables:
-            tables[table.replace(f'{current_schema}.', '')] = base_model.metadata.tables[table]
-        base_model_metadata_tables = FacadeDict(tables)
-        setattr(base_model.metadata, 'tables', base_model_metadata_tables)
+    def __get_sqlite_type_for_column(column):
+        column_type = column.type
+        if isinstance(column_type, BIGINT):
+            column.type = INTEGER()
+        elif isinstance(column_type, DATETIME):
+            column.type = SQLITE_DATETIME()
+        elif isinstance(column_type, DATETIME2):
+            column.type = SQLITE_DATETIME()
+        elif isinstance(column_type, DATE):
+            column.type = SQLITE_DATE()
+        elif isinstance(column_type, DECIMAL):
+            column.type = SQLITE_DECIMAL()
+        elif isinstance(column_type, TINYINT):
+            column.type = SMALLINT()
